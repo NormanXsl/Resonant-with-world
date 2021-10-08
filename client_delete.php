39dc3eaca780bc38e4b4f299e2b92be7b73feb1d
Nak <58125177+Sovathanak@users.noreply.github.com>
2021-10-08 21:40:44 +0700

    delete minor changes

--- client_delete.php
+++ client_delete.php
@@ -4,6 +4,19 @@ require('connection.php');
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['client_id'])) {
+    // Delete image files first
+    $query = "SELECT * FROM `client` WHERE `client_id` = ?";
+    $stmt = $dbh->prepare($query);
+    $stmt->execute($_POST['client_id']);
+    while ($image = $stmt->fetchObject()) {
+        $fileFullPath = "product_images" . DIRECTORY_SEPARATOR . $image->filename;
+        unlink($fileFullPath);
+    }
+    
+    $query = "DELETE FROM `photoshoot` WHERE `client_fk` in (" . $query_placeholders . ")";
+    $stmt = $dbh->prepare($query);
+    $stmt->execute($_POST['product_ids']);
+
     $query = "DELETE FROM `client` WHERE `client_id` = ?";
     $stmt = $dbh->prepare($query);
     $stmt->execute([$_POST['client_id']]);
